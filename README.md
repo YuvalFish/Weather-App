1156cd9516c2c6630173674e418b666a3fe26d44
Yuval Fishbine <YuvalFish12@gmail.com>
2024-08-11 20:13:25 +0300

    add website design post route

---
+++ README.md
@@ -0,0 +1,2 @@
+# City weather checking app
+Practicing my full stack skills so far and trying to make a weather checking app usin Weatherstack API
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import axios from "axios";
-import { fileURLToPath } from 'url'
-import path from 'path'
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import { fileURLToPath } from 'url';
+import path from 'path';
+import 'dotenv/config';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = 3000;
+const apiKey = process.env.API_KEY;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -13,6 +15,16 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.render(__dirname + "/views/index.ejs");
 })
+
+app.post("/check", async (req,res) => {
+    const userCity = req.body.city;
+    console.log(apiKey);
+    console.log(userCity);
+
+    const response = await axios.get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${userCity}`);
+    console.log(response.data);
+    res.render(__dirname + "/views/index.ejs");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
