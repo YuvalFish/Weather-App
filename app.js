2b3b9fab1ae3e7c87476a44702f07f4abbdbb201
Yuval Fishbine <YuvalFish12@gmail.com>
2024-08-12 20:36:58 +0300

    add API functionaly, finish touch of the design

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { response } from "express";
 import axios from "axios";
 import { fileURLToPath } from 'url';
 import path from 'path';
 import 'dotenv/config';
+import fs from 'fs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -12,18 +13,84 @@ const apiKey = process.env.API_KEY;
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render(__dirname + "/views/index.ejs");
-})
+app.get("/", async (req, res) => {
+    
+    /* Practice the API with JSON data instead of wasting the API key
+    const data = JSON.parse(fs.readFileSync('./response.json',
+        { encoding: 'utf8', flag: 'r' }));
+        
+    console.log(data);
+    const city_name = data.location.name;
+    const city_temp = data.current.temperature;
+    const description = data.current.weather_descriptions[0];
+    const feelsLike = data.current.feelslike;
+
+    console.log(description);
+    res.render(__dirname + "/views/index.ejs", { 
+        cityName: city_name,
+        temp: city_temp,
+        emoji: getEmoji(description),
+        feels: feelsLike
+    }); */
+
+    try {
+        const response = await axios.get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=Tel Aviv`);
+        // console.log(response.data);
+
+        const city_name = response.data.location.name;
+        const city_temp = response.data.current.temperature;
+        const description = response.data.current.weather_descriptions[0];
+        const feelsLike = response.data.current.feelslike;
+
+        res.render(__dirname + "/views/index.ejs", { 
+            cityName: city_name,
+            temp: city_temp,
+            emoji: getEmoji(description),
+            feels: feelsLike
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.render(__dirname + "/views/index.ejs");
+    }
+
+});
+
+function getEmoji(weather) {
+    switch (weather) {
+        case "Sunny":
+            return '🌤️';
+            break;
+        case "Overcast":
+            return '☁️';
+            break;
+        default:
+            break;
+    }
+}
 
 app.post("/check", async (req,res) => {
     const userCity = req.body.city;
-    console.log(apiKey);
-    console.log(userCity);
+    try {
+        const response = await axios.get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${userCity}`);
+        // console.log(response.data);
 
-    const response = await axios.get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${userCity}`);
-    console.log(response.data);
-    res.render(__dirname + "/views/index.ejs");
+        const city_name = response.data.location.name;
+        const city_temp = response.data.current.temperature;
+        const description = response.data.current.weather_descriptions[0];
+        const feelsLike = response.data.current.feelslike;
+
+        res.render(__dirname + "/views/index.ejs", { 
+            cityName: city_name,
+            temp: city_temp,
+            emoji: getEmoji(description),
+            feels: feelsLike
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.render(__dirname + "/views/index.ejs");
+    }
 });
 
 app.listen(PORT, () => {
